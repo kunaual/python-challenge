c9cb60b9adac1d977d916bab5000c4d729a45cfb
kunaual <aekunau@gmail.com>
2021-01-17 18:25:27 -0600

    work on pypoll main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,21 @@
-print("Hello pyPoll!")
+import os
+import pandas as pd
 
-q = 2
+filepath = os.path.join('Resources','election_data.csv')
+
+eldata_df = pd.read_csv(filepath, encoding ='UTF-8')
+print(eldata_df.shape)
+totalVotes=eldata_df["Voter ID"].count()
+# get the counts for each candidate, make it a df, reset index and rename columns
+candList_df=eldata_df["Candidate"].value_counts().to_frame().reset_index().rename(columns = {"index":"Candidate","Candidate":"NumVotes"})
+#candList_df=candList.rename(columns = {"index":"Candidate","Candidate":"NumVotes"})
+
+pctVote=(candList_df["NumVotes"]/totalVotes)*100
+
+candList_df["Pct"]=pctVote
+print(candList_df)
+#  * The total number of votes cast
+#  * A complete list of candidates who received votes
+#  * The percentage of votes each candidate won
+#  * The total number of votes each candidate won
+# * The winner of the election based on popular vote.
